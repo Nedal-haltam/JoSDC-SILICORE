f76cbca69077b54012f3d38784128cc388effc5a
Qabas <168840541+QabasAK@users.noreply.github.com>
2024-10-02 15:37:12 +0300

    Update SignExtender.v

--- singlecycle/Qualification Phase Modules/SignExtender.v
+++ singlecycle/Qualification Phase Modules/SignExtender.v
@@ -13,20 +13,3 @@ module SignExtender(in, out);
 	assign out = {{16{in[15]}}, in};
 	
 endmodule
-
-/*
-//additional testbench
-module tb();
-  reg [15:0] in;
-  wire [31:0] out;
-  
-  SignExtender testing(in, out);
-  
-  initial 
-    begin
-      in=16'b1111111111111111;
-      #10 $display("%b", out);       
-    end
-
-endmodule
-*/
