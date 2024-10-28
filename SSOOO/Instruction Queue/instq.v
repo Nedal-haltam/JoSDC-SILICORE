255679530872b9721326eca0eac94cacd45c4a80
Nedal Abu Haltam <133881380+Nedal-haltam@users.noreply.github.com>
2024-10-28 23:12:15 +0300

    Update instq.v

--- SSOOO/Instruction Queue/instq.v
+++ SSOOO/Instruction Queue/instq.v
@@ -1,1 +1,29 @@
+module instq
+(
+    input  [31:0] PC,
+    output [ 5:0] opcode, funct,
+    output [ 4:0] rs, rt, rd, shamt,
+    output [15:0] immediate,
+    output [25:0] address
+);
 
+
+reg [31:0] InstMem [63:0];
+
+wire [31:0] inst;
+
+assign inst = InstMem[PC];
+
+assign opcode    = inst[31:26];
+assign funct     = inst[5:0];
+assign rs        = inst[25:21];
+assign rt        = inst[20:16];
+assign rd        = inst[15:11];
+assign shamt     = inst[10:6];
+assign immediate = inst[15:0];
+assign address   = inst[25:0];
+
+
+
+
+endmodule
