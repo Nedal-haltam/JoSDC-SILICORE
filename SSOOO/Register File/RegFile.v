2bc5904b326e61236c9b1646a18068a94f19006c
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2024-10-29 19:12:03 +0300

    update register file and tb

--- SSOOO/Register File/RegFile.v
+++ SSOOO/Register File/RegFile.v
@@ -4,22 +4,56 @@ module RegFile
     parameter ROB_Entry_WIDTH = 5
  )
 (
-    input [ROB_Entry_WIDTH - 1:0] WP1_ROBEN, 
-    input [4:0]                   WP1_DRindex, 
+    input clk, rst,
+    input WP1_WR, WP1_WR_IQ,
+    input [ROB_Entry_WIDTH - 1:0] WP1_ROBEN, WP1_ROBEN_IQ, 
+    input [4:0]                   WP1_DRindex, WP1_DRindex_IQ, 
     input [31:0]                  WP1_Data,
+
+
 
     input [4:0]   RP1_index1, RP1_index2,
     output [31:0] RP1_Reg1, RP1_Reg2
+    // for testing purposes
+    , output [(1 << ROB_Entry_WIDTH) - 1 : 0] output_ROBEN_test
 );
 
 
 
 reg [31:0] Regs [31:0];
-reg [(2 ** ROB_Entry_WIDTH) - 1 : 0] Reg_ROBEN [31:0];
+reg [(1 << ROB_Entry_WIDTH) - 1 : 0] Reg_ROBEs [31:0];
 
+assign output_ROBEN_test = Reg_ROBEs[WP1_DRindex_IQ];
 
 assign RP1_Reg1 = Regs[RP1_index1];
 assign RP1_Reg2 = Regs[RP1_index2];
+
+
+integer i;
+always@(posedge clk , posedge rst) begin
+
+if (rst) begin
+for(i = 0; i < 32; i++) begin
+    Regs[i] <= 0;
+end
+end
+
+else if (WP1_WR && WP1_DRindex != 5'd0 && Reg_ROBEs[WP1_DRindex] == WP1_ROBEN)
+    Regs[WP1_DRindex] <= WP1_Data;
+end
+
+integer j;
+always@(negedge clk , posedge rst) begin
+
+if (rst) begin
+for(j = 0; j < 32; j++) begin
+    Reg_ROBEs[j] <= 0;
+end
+end
+
+else if (WP1_DRindex_IQ != 5'd0 && WP1_WR_IQ)
+    Reg_ROBEs[WP1_DRindex_IQ] <= WP1_ROBEN_IQ;
+end
 
 
 
--- SSOOO/Register File/RegFile_tb.v
+++ SSOOO/Register File/RegFile_tb.v
@@ -4,24 +4,55 @@ module RegFile_tb;
 
 parameter ROB_Entry_WIDTH = 5;
 
-reg [ROB_Entry_WIDTH - 1:0] WP1_ROBEN;
-reg [4:0]                   WP1_DRindex;
+reg clk = 0, rst = 0;
+reg WP1_WR = 0, WP1_WR_IQ = 0;
+reg [ROB_Entry_WIDTH - 1:0] WP1_ROBEN, WP1_ROBEN_IQ;
+reg [4:0]                   WP1_DRindex, WP1_DRindex_IQ;
 reg [31:0]                  WP1_Data;
 reg [4:0]   RP1_index1, RP1_index2;
 wire [31:0] RP1_Reg1, RP1_Reg2;
 
+wire [(1 << ROB_Entry_WIDTH) - 1 : 0] output_ROBEN_test;
+
 RegFile #(.ROB_Entry_WIDTH(ROB_Entry_WIDTH)) dut  
 (
+.clk(clk),
+.rst(rst),
 
+.WP1_WR(WP1_WR),
+.WP1_WR_IQ(WP1_WR_IQ),
 .WP1_ROBEN(WP1_ROBEN), 
 .WP1_DRindex(WP1_DRindex), 
 .WP1_Data(WP1_Data),
+.WP1_ROBEN_IQ(WP1_ROBEN_IQ),
+.WP1_DRindex_IQ(WP1_DRindex_IQ),
 .RP1_index1(RP1_index1), 
 .RP1_index2(RP1_index2),
 .RP1_Reg1(RP1_Reg1),
 .RP1_Reg2(RP1_Reg2)
 
+, .output_ROBEN_test(output_ROBEN_test)
 );
+
+// always #1 clk <= ~clk;
+integer i, j;
+initial begin
+rst = 1; #2 rst = 0;
+
+$monitor("RP1_index1 = %d , RP1_Reg1 = %d", RP1_index1, RP1_Reg1);
+for (i = 0; i < 32; i++) begin
+    RP1_index1 <= i; #1;
+end
+
+$monitor("WP1_DRindex_IQ = %d , output_ROBEN_test = %d", WP1_DRindex_IQ, output_ROBEN_test);
+for (j = 0; j < 32; j++) begin
+    WP1_DRindex_IQ <= j; #1; 
+end
+
+// it is important to put $stop or $finish to terminate the simulation becase 
+// the line: always #1 clk <= ~clk;, will force the simulation to cotinue and you should exit manually
+#1 $finish;
+end
 
 
 endmodule
