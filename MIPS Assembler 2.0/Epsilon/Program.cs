72c899080e16c08b18cbab6bd42c7f0c1cb22f14
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2024-11-08 14:33:43 +0300

    Create project Epsilon
    Creating a program to write higher code and convert it to instruction and then machine code

---
+++ MIPS Assembler 2.0/Epsilon/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using static Epsilon.Macros;
+
+
+namespace Epsilon
+{
+    public static class Macros
+    {
+        public static void cout(string str, params object[] args)
+        {
+            Console.WriteLine(str, args);
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
