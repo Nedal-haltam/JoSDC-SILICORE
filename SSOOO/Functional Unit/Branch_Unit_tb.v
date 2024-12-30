87ba8ccd7ad3cacb9d2af85b3c3b27932ebf7ca2
Alanoud Alsalem <168756387+AlanoudAlsalem@users.noreply.github.com>
2024-12-30 12:51:58 +0300

    Update Branch_Unit_tb.v

--- SSOOO/Functional Unit/Branch_Unit_tb.v
+++ SSOOO/Functional Unit/Branch_Unit_tb.v
@@ -1,22 +1,26 @@
 `include "Branch_Unit.v"
 module Branch_Unit_tb;
     reg [31:0] operand1, operand2;
+    reg [4:0] ROBEN_in;
     wire result;
+    wire [4:0] ROBEN_out;
 
     Branch_Unit DUT(
         .operand1(operand1),
         .operand2(operand2),
-        .equal(result)
+        .ROBEN_in(ROBEN_in),
+        .equal(result),
+        .ROBEN_out(ROBEN_out)
     );
 
     initial begin
-        operand1 = 32'habd; operand2 = 32'h123; #1
-        $display("%d & %d equal = %b", operand1, operand2, result);
-        operand1 = 32'habd; operand2 = 32'habd; #1
-        $display("%d & %d equal = %b", operand1, operand2, result);
-        operand1 = 32'habd156; operand2 = 32'habd; #1
-        $display("%d & %d equal = %b", operand1, operand2, result);
-        operand1 = 32'h0; operand2 = 32'h0; #1
-        $display("%d & %d equal = %b", operand1, operand2, result);
+        operand1 = 32'habd; operand2 = 32'h123; ROBEN_in = 5'b1; #1
+        $display("%d & %d equal = %b, ROBEN_out = %d", operand1, operand2, result, ROBEN_out);
+        operand1 = 32'habd; operand2 = 32'habd; ROBEN_in = 5'b10; #1
+        $display("%d & %d equal = %b, ROBEN_out = %d", operand1, operand2, result, ROBEN_out);
+        operand1 = 32'habd156; operand2 = 32'habd; ROBEN_in = 5'b11; #1
+        $display("%d & %d equal = %b, ROBEN_out = %d", operand1, operand2, result, ROBEN_out);
+        operand1 = 32'h0; operand2 = 32'h0; ROBEN_in = 5'b100; #1
+        $display("%d & %d equal = %b, ROBEN_out = %d", operand1, operand2, result, ROBEN_out);
     end
 endmodule
