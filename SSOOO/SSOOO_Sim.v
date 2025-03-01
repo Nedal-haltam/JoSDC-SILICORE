328d1c0c77c3aada3aed877530ae5347ae3f6a73
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-03-01 17:34:15 +0300

    OOO CPU passed all but threesumproblem

--- SSOOO/SSOOO_Sim.v
+++ SSOOO/SSOOO_Sim.v
@@ -3,7 +3,7 @@
 `define ONE_CLK (2 * `HALF_CYCLE)
 `define ADVANCE_N_CYCLE(N) #(`ONE_CLK * N);
 
-`define MAX_CLOCKS (100 * 1000 * 1000)
+`define MAX_CLOCKS (200 * 1000 * 1000)
 `define reset 2 * `ONE_CLK
 
 
